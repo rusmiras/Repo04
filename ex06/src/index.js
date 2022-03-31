b358bfa4e84b8f12088717f9d5b79523f215a0e9
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-03-31 11:37:53 +0200

    Dividing two decimals

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo) {
+    // only change code below this line
+    var myQuotient = numOne / numTwo;
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0));  // Change this line
+console.log(main(16.5, 5.5));  // Change this line
+console.log(main(102.0, 25.5));  // Change this line
+module.exports = main;
